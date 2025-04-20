336470829fbad691939d4ddbcd330ea1a101c30f
Sauhaan Sharma <sharmasauhaan1@gmail.com>
2025-04-20 15:01:45 +0530

    contact page updated

---
+++ script.js
@@ -0,0 +1,184 @@
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
+
+const products = [
+  {
+    id: 1,
+    title: "Air Force",
+    price: 119,
+    colors: [
+      {
+        code: "black",
+        img: "./images/air.png",
+      },
+      {
+        code: "darkblue",
+        img: "./images/air2.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Air Jordan",
+    price: 149,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./images/jordan.png",
+      },
+      {
+        code: "green",
+        img: "./images/jordan2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Blazer",
+    price: 109,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./images/blazer.png",
+      },
+      {
+        code: "green",
+        img: "./images/blazer2.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Crater",
+    price: 129,
+    colors: [
+      {
+        code: "black",
+        img: "./images/crater.png",
+      },
+      {
+        code: "lightgray",
+        img: "./images/crater2.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Hippie",
+    price: 99,
+    colors: [
+      {
+        code: "gray",
+        img: "./images/hippie.png",
+      },
+      {
+        code: "black",
+        img: "./images/hippie2.png",
+      },
+    ],
+  },
+];
+
+let choosenProduct = products[0];
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+
+menuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    //change the current slide
+    wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+    //change the choosen product
+    choosenProduct = products[index];
+
+    //change texts of currentProduct
+    currentProductTitle.textContent = choosenProduct.title;
+    currentProductPrice.textContent = "$" + choosenProduct.price;
+    currentProductImg.src = choosenProduct.colors[0].img;
+
+    //assing new colors
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = choosenProduct.colors[index].code;
+    });
+  });
+});
+
+currentProductColors.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currentProductImg.src = choosenProduct.colors[index].img;
+  });
+});
+
+currentProductSizes.forEach((size, index) => {
+  size.addEventListener("click", () => {
+    currentProductSizes.forEach((size) => {
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
+    });
+    size.style.backgroundColor = "black";
+    size.style.color = "white";
+  });
+});
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  payment.style.display = "none";
+});
+
+document.getElementById("BuyNow").addEventListener("click", function(){
+  // Get selected color
+  let selectedColorIndex = 0;
+  currentProductColors.forEach((color, idx) => {
+    if (color.style.border === "2px solid #000" || color.style.outline === "2px solid #000") {
+      selectedColorIndex = idx;
+    }
+  });
+
+  // Get selected size
+  let selectedSize = null;
+  currentProductSizes.forEach((size) => {
+    if (size.style.backgroundColor === "black") {
+      selectedSize = size.textContent;
+    }
+  });
+
+  // Fallbacks
+  if (!selectedSize) selectedSize = currentProductSizes[0].textContent;
+
+  // Prepare cart item
+  const cartItem = {
+    id: choosenProduct.id,
+    title: choosenProduct.title,
+    price: choosenProduct.price,
+    color: choosenProduct.colors[selectedColorIndex].code,
+    img: choosenProduct.colors[selectedColorIndex].img,
+    size: selectedSize,
+    quantity: 1
+  };
+
+  // Get cart from localStorage
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  // Check if item already exists (same id, color, size)
+  const existingIndex = cart.findIndex(item =>
+    item.id === cartItem.id &&
+    item.color === cartItem.color &&
+    item.size === cartItem.size
+  );
+  if (existingIndex > -1) {
+    cart[existingIndex].quantity += 1;
+  } else {
+    cart.push(cartItem);
+  }
+});
